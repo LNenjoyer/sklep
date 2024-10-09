8a3cdf0cf0b3a16e03c15e8a5ca2f6bb06e8b621
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-10-09 10:21:42 +0200

    zmiany

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import './sklep';
+import Sklep from "./sklep";
+import telefon from './telefon.png';
 
-function App() {
-  return (
+
+const Produkt =[
+  {name:"XIAOMI POCO", photo:telefon, desc:"telefon"},
+    {name:"XIAOMI POCO", photo:telefon, desc:"telefon"},
+    {name:"XIAOMI POCO", photo:telefon, desc:"telefon"}
+]
+
+const App = () =>(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+        <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
+               name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
+               name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
+        />
+        <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
+               name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
+               name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
+        />
+        <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
+               name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
+               name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
+        />
+
+
+
+    </div>
+)
 export default App;
---
+++ src/sklep.jsx
@@ -0,0 +1,28 @@
+import './sklep.css';
+import React from "react";
+
+const Sklep = (props)=>(
+<table>
+    <tbody>
+        <tr>
+            <td>
+                <img src={props.photo1} alt={props.name1}/>
+                {props.name1}
+                {props.desc1}
+            </td>
+            <td>
+                <img src={props.photo2} alt={props.name2}/>
+                {props.name2}
+                {props.desc2}
+            </td>
+            <td>
+                <img src={props.photo3} alt={props.name3}/>
+                {props.name3}
+                {props.desc3}
+            </td>
+        </tr>
+    </tbody>
+
+</table>
+);
+export default Sklep;
