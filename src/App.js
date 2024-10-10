78033217487cc5f555ec8d43c01518ca5a2affc9
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-10-10 09:15:25 +0200

    jest ok

--- src/App.js
+++ src/App.js
@@ -11,20 +11,33 @@ const Produkt =[
     {name:"MSI 4070 TI SUPER 16G", photo:grafika, desc:"4700.00"}
 ]
 const App = () =>(
-    <div className="App">
+    <div id="ok">
+
+        <div className="lewa">
+            <p><a href="inne1">inne1</a></p>
+            <p><a href="inne2">inne2</a></p>
+            <p><a href="inne3">inne3</a></p>
+            <p><a href="inne4">inne4</a></p>
+            <p><a href="inne5">inne5</a></p>
+            <p><a href="inne6">inne6</a></p>
+            <p><a href="inne7">inne7</a></p>
+        </div>
+        <div className="App">
 
         <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
-               name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
-               name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
-        />
-        <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
-               name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
-               name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
-        />
-        <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
-               name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
-               name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
-        />
+                   name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
+                   name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
+            />
+            <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
+                   name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
+                   name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
+            />
+            <Sklep name1={Produkt[0].name} photo1={Produkt[0].photo} desc1={Produkt[0].desc}
+                   name2={Produkt[1].name} photo2={Produkt[1].photo} desc2={Produkt[1].desc}
+                   name3={Produkt[2].name} photo3={Produkt[2].photo} desc3={Produkt[2].desc}
+            />
+        </div>
     </div>
+
 )
 export default App;
